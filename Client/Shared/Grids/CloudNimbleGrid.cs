ab5208674d631fcb0f1c130f2010f51ea9a97548
Robert McLaws <1657085+robertmclaws@users.noreply.github.com>
2023-01-31 13:44:42 -0500

    Attempting to get this to wotk on .NET 5 (last known good)

---
+++ Client/Shared/Grids/CloudNimbleGrid.cs
@@ -0,0 +1,16 @@
+﻿using CloudNimble.EasyAF.Core;
+using System;
+
+namespace RadzenGridProblem.Client.Shared.Grids
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TItem"></typeparam>
+    /// <remarks>RWM: Generic type constraints have to be specified in partial classes.</remarks>
+    public partial class CloudNimbleGrid<TItem> where TItem: IIdentifiable<Guid>
+    {
+    }
+
+}
